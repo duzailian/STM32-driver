d95f234c9f9d4a9039f246b14b0df281011bce2e
duzailian <duzailian@qq.com>
2020-11-22 23:30:32 +0800

    1. 完成基于python和Makefile的自动生成IAR工程功能开发并测试通过

---
+++ prj/scripts/project_cfg.py
@@ -0,0 +1,111 @@
+Projects = {
+    "firmware": [
+        {
+            'name':
+            'comm-setting',
+            'include': [
+                "src/app", "src/bsp", "src/driver", "src/lib", "src/os",
+                "src/os/ucos/cfg/", "src/os/ucos/Task/", "src/os/ucos/uC-CPU/",
+                "src/os/ucos/uC-CPU/ARM-Cortex-M3/IAR/", "src/os/ucos/uC-LIB/",
+                "src/os/ucos/uCOS-III/Ports/ARM-Cortex-M3/Generic/IAR",
+                "src/os/ucos/uCOS-III/Source/",
+            ],
+            "define": ["STM32F10X_CL", "USE_STDPERIPH_DRIVER", "DEBUG_EN"],
+        },
+        {
+            'name': 'app',
+            'src': [
+                'src/app/main.c',
+            ],
+        },
+        {
+            'name': 'bsp',
+            'src': [
+                'src/bsp/bsp.c',
+                'src/bsp/nrf24l01/nrf24l01.c',
+            ],
+        },
+        {
+            'name':
+            'driver',
+            'src': [
+                'src/driver/driver.c',
+                'src/driver/dma/dma.c',
+                'src/driver/exti/exti.c',
+                'src/driver/gpio/gpio.c',
+                'src/driver/interrupt/interrupt.c',
+                'src/driver/log/log.c',
+                'src/driver/rcc/rcc.c',
+                'src/driver/spi/spi.c',
+                'src/driver/systick/systick.c',
+                'src/driver/usart/usart.c',
+            ],
+        },
+    ],
+    "boot": [
+        {
+            'name':
+            'comm-setting',
+            'include': [
+                "src/app", "src/bsp", "src/driver", "src/lib", "src/os",
+                "src/os/ucos/cfg/", "src/os/ucos/Task/", "src/os/ucos/uC-CPU/",
+                "src/os/ucos/uC-CPU/ARM-Cortex-M3/IAR/", "src/os/ucos/uC-LIB/",
+                "src/os/ucos/uCOS-III/Ports/ARM-Cortex-M3/Generic/IAR",
+                "src/os/ucos/uCOS-III/Source/",
+                 "src/lib/ST/inc",
+            ],
+            "define":
+            ["STM32F10X_CL", "USE_STDPERIPH_DRIVER", "DEBUG_EN", "BOOT_PRJ"],
+        },
+        {
+            'name': 'app',
+            'src': [
+                'src/app/iap/iap.c',
+                'src/app/main.c',
+            ],
+        },
+        {
+            'name': 'bsp',
+            'src': [
+                'src/bsp/bsp.c',
+                'src/bsp/nrf24l01/nrf24l01.c',
+            ],
+        },
+        {
+            'name':
+            'driver',
+            'src': [
+                'src/driver/driver.c',
+                'src/driver/dma/dma.c',
+                'src/driver/exti/exti.c',
+                'src/driver/gpio/gpio.c',
+                'src/driver/interrupt/interrupt.c',
+                'src/driver/log/log.c',
+                'src/driver/rcc/rcc.c',
+                'src/driver/spi/spi.c',
+                'src/driver/systick/systick.c',
+                'src/driver/usart/usart.c',
+            ],
+        },
+        {
+            'name':
+            'lib',
+            'src': [
+                'src/lib/CMSIS/CM3/CoreSupport/core_cm3.c',
+                'src/lib/CMSIS/CM3/DeviceSupport/ST/STM32F10x/startup/iar/startup_stm32f10x_cl.s',
+                'src/lib/library/crc8/crc8.c',
+                'src/lib/ST/src/stm32f10x_it.c',
+                'src/lib/ST/src/stm32f10x_rcc.c',
+                'src/lib/ST/src/system_stm32f10x.c',
+            ],
+        },
+        {
+            'name': 'os',
+            'src': [
+                'src/os/ucos/uC-CPU/ARM-Cortex-M3/IAR/cpu_a.asm',
+            ],
+        },
+    ]
+}
+iar_ogcmenu = "STM32F107VC ST STM32F107VC"
+global_ldflags = "--config platform/board/stm32f103rb-nucleo/stm32f103xb_flash.icf --silent"
